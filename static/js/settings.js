911913cde3190f7f6be4b70a28abf840285b1be3
Rongchuan Zhang <alexzhan12@gmail.com>
2012-03-10 21:21:13 +0800

    add change username and domain module in account.html

--- static/js/settings.js
+++ static/js/settings.js
@@ -32,3 +32,26 @@ function subch(){
     }
     if(!mark) return false;
 }
+function subac(name, domain) {
+    var controls=new Array("name","domain");
+    for (control in controls){
+        $("#"+controls[control]+"control").removeClass("error");
+        $("#"+controls[control]+"help").html("");
+    }
+    $("#namehelp").html("中英文、数字皆可，不能少于2个字符，一个月只能修改一次");
+    $("#domainhelp").html("可以使用英文或数字，不能少于2个字符，一个月只能修改一次");
+    var mark = 1;
+    if($("#name").val().length == 0) {
+        $("#namecontrol").addClass("error");
+        $("#namehelp").html("用户名不能为空");
+        mark = 0;
+    }
+    if($("#domain").val().length == 0) {
+        $("#domaincontrol").addClass("error");
+        $("#domainhelp").html("个性域名不能为空");
+        mark = 0;
+    }
+    if($("#name").val().length != 0 && $("#name").val() == name && $("#domain").val().length != 0 && $("#domain").val() == domain)
+        mark = 0;
+    if(!mark) return false;
+}
