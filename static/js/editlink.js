e5f3267562250e0c2762b0c61c9d84861666a536
Rongchuan Zhang <alexzhan12@gmail.com>
2012-03-14 15:46:42 +0800

    finish editlink url and js

---
+++ static/js/editlink.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+    if($("#sug").text() == '0'){
+        modaltoggle(0);
+    }
+});
+function cansug() {
+    if($("#sug").text() != '0')
+        return true;
+    $.ajax({
+    type:'POST',
+    url:'/cansug',
+    data:{_xsrf:getCookie('_xsrf')},
+    success:function(data){
+        $("#sug").text("1");
+    }       
+    });
+}
+function modaltoggle(option) {
+    if(option) 
+        $(".suggestbox").hide();
+    var ie = document.all ? 1 : 0;
+    if(ie){
+        $("#cr").hide();
+        $("#ie").show();
+    }
+    else{
+        $("#ie").hide();
+        $("#cr").show();
+    }
+    $('#crModal').modal('toggle');
+    return false;
+}
+function editlink() {
+    var linktype = 0;
+    if ($("#secret").attr("checked")=="checked"){
+        linktype = 1;
+    }
+    var linkurl = $("#url").val();
+    var linktitle = $("#title").val();
+    var linksummary = $("#summary").val();
+    var linktag = $("#tag").val();
+    $("#linkurl").removeClass("error");
+    if(linkurl.length == 0) {
+        $("#linkurl").removeClass();
+        $("#linkurl").addClass("control-group error");
+        return false;
+    }
+    $.ajax({
+    type:'POST',
+    url:'/link/edit',
+    data:{
+        linkurl:linkurl,
+        linktitle:linktitle,
+        linksummary:linksummary,
+        linktag:linktag,
+        _xsrf:getCookie('_xsrf')
+    },
+    success:function(data){
+    }       
+    });
+    return false;
+}
